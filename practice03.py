1bb0fd5c3d8ea24b0a7852cc7acf044241d6e51d
Haruki Shimoji <shimoji@jnlp.org>
2015-11-04 16:30:33 +0900

    python3を使用するようにした

--- practice03.py
+++ practice03.py
@@ -1,11 +1,10 @@
+#! /usr/bin/python3
 # coding: utf-8
-
-from __future__ import print_function, unicode_literals
 
 def main():
     text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
     words = text.split(" ")
-    print(map(len, [word.strip(",.") for word in words]))
+    print(*map(len, [word.strip(",.") for word in words]))
 
 if __name__ == "__main__":
     main()
