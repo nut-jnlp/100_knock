fc76ea66221994935813fabe3ff5c588d21295fe
Haruki Shimoji <shimoji@jnlp.org>
2015-11-04 16:24:37 +0900

    文字列をスライスするように変更した

--- practice01.py
+++ practice01.py
@@ -1,10 +1,9 @@
+#! /usr/bin/python3
 # coding: utf-8
-
-from __future__ import print_function, unicode_literals
 
 def main():
     string = "パタトクカシーー"
-    print("".join(c for i, c in enumerate(string) if i % 2 == 0))
+    print(string[::2])
 
 if __name__ == "__main__":
     main()
