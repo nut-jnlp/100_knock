68202f3140fcb42b235aa61f3ce09887b8ebfc41
Haruki Shimoji <shimoji@jnlp.org>
2015-11-04 16:42:13 +0900

    python3を使用するようにした

--- practice08.py
+++ practice08.py
@@ -1,13 +1,12 @@
+#! /usr/bin/python3
 # coding: utf-8
-
-from __future__ import print_function, unicode_literals
 
 def main():
     plain = "Hello, World!"
     print(cipher(plain))
 
 def cipher(s):
-    return "".join([c if not c.islower() else unichr(219) for c in s])
+    return "".join([c if not c.islower() else chr(219) for c in s])
 
 if __name__ == "__main__":
     main()
