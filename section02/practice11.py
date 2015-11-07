7400536f7ed20d689e6d280569bb4c2dd88303a1
Haruki Shimoji <shimoji@jnlp.org>
2015-11-07 23:04:11 +0900

    print関数のキーワード引数endに空文字列を渡すことで最後に改行しないようにした。

--- section02/practice11.py
+++ section02/practice11.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 def main():
-    print(*map(lambda line: line.replace('\t', ' '), open("hightemp.txt")), sep="")
+    print(*map(lambda line: line.replace('\t', ' '), open("hightemp.txt")), sep="", end="")
 
 if __name__ == "__main__":
     main()
