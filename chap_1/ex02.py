c96ba5d9983e56ab56fc3be201fa5ce4479931fd
Maruyama <maruyama@jnlp.org>
2016-11-16 20:49:53 +0900

    連結した文字列から表示するように変更しました。

--- chap_1/ex02.py
+++ chap_1/ex02.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-word1="パトカー"
-word2="タクシー"
+word1 = "パトカー"
+word2 = "タクシー"
 
-for(s1,s2) in zip(word1,word2):
-    print(s1+s2,end="")
-
+s = [s1+s2 for s1, s2 in zip(word1, word2)]
+print("".join(s))
