9a036b8bc0a513fb689e70d1e8ab76eb38ffc521
inaoka <in@okayu.jp>
2016-11-16 20:31:31 +0900

    q02 を内包表記を用いた方法に修正。

--- chap01/q02.py
+++ chap01/q02.py
@@ -3,10 +3,6 @@
 word1 = "パトカー"
 word2 = "タクシー"
 
-char_list = []
-for t in zip(word1, word2):
-    for c in t:
-        char_list.append(c)
-merged_word = "".join(char_list)
+merged_word = "".join(["".join(t) for t in zip(word1, word2)])
 
 print(merged_word)
