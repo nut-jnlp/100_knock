283362d69505001c5b26cb14f2b076d857d08c81
inaoka <in@okayu.jp>
2016-11-16 20:32:19 +0900

    q03 を内包表記を用いた方法に修正。

--- chap01/q03.py
+++ chap01/q03.py
@@ -3,8 +3,8 @@
 sentence =\
     "Now I need a drink, alcoholic of course, "\
     "after the heavy lectures involving quantum mechanics."
-words = sentence.replace(",", "").replace(".", "")
-nums = []
-for word in words.split():
-    nums.append(len(word))
+
+words = sentence.replace(",", "").replace(".", "").split()
+nums = [len(word) for word in words]
+
 print(nums)
