b2b2a9e39409011bc525b1430c59fb1906a8ff9b
inaoka <in@okayu.jp>
2016-11-18 17:30:31 +0900

    文章を単語リストにする

---
+++ chap01/q04.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+sentence = """\
+"Hi He Lied Because Boron Could Not Oxidize Fluorine. \
+New Nations Might Also Sign Peace Security Clause. Arthur King Can."""
+
+words = sentence.replace(",", "").replace(".", "").split(" ")
