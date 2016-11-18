06640f529005ac88d2440de7f560a8c96c1d5075
inaoka <in@okayu.jp>
2016-11-18 16:42:08 +0900

    タプルの結合をchainで行うよう修正

--- chap01/q02.py
+++ chap01/q02.py
@@ -1,8 +1,9 @@
 # coding: utf-8
+from itertools import chain
 
 word1 = "パトカー"
 word2 = "タクシー"
 
-merged_word = "".join(["".join(t) for t in zip(word1, word2)])
+merged_word = "".join(chain(*zip(word1, word2)))
 
 print(merged_word)
