d3be080994a3447f0ebdbad893023680ff0228e3
youichiro <cinnamon416@gmail.com>
2016-11-19 15:03:19 +0900

    enumerate(words, start=1)

--- chap_01/q04.py
+++ chap_01/q04.py
@@ -6,16 +6,17 @@ s = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also
 words = s.split(" ")
 #print(words)
 
-n = [1,5,6,7,8,9,15,16,19]
+n = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
 dict = {}
 
-for number,string in enumerate(words):
-	if number+1 in n:
-		dict[number+1] = string[:1:1]
-	else:
-		dict[number+1] = string[:2:1]
+for number, string in enumerate(words, start=1):
+    if number in n:
+	    dict[number] = string[:1:1]
+    else:
+	    dict[number] = string[:2:1]
 
-for key,value in dict.items():
-	print("key: % d \t value: %s" % (key,value))
+for key, value in dict.items():
+    print("key: % d \t value: %s" % (key,value))
+
 
