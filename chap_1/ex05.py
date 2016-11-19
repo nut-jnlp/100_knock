91351310a60ac12688ec8627b018263c4465ca3c
Maruyama <maruyama@jnlp.org>
2016-11-19 17:49:25 +0900

    修正しました

--- chap_1/ex05.py
+++ chap_1/ex05.py
@@ -14,24 +14,4 @@ for num in ngram_str("I am a NLPer", 2):
 print("\n")
 
 for num in ngram_word("I am a NLPer", 2):
-    print(num) 
-
-#def n_gram_word(s, n):
-#    s = s.split(" ")
-#    if(n >= len(s)):
-#       return("Can't split")
-#   else:
-#       length = len(s)
-#        return [s[i:i+n] for i in range(0, length, n)]
-#
-
-#if n_gram_str(s, n):
-#    if(n >= len(s)):
-#        return("Can't split")
-#    else:
-#        length = len(s)
-#        return [s[i:i+n] for i in range(0, length, n)]
-
-
-#print(n_gram_str(s, n))
-
+    print(num)
