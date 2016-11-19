c36c30c4d52bea4b5f201ba0c9372667c3de2d72
inaoka <in@okayu.jp>
2016-11-19 18:11:13 +0900

    with構文を使用したリソース確保に修正

--- chap02/q12.py
+++ chap02/q12.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 import fileinput
 
-f_in = fileinput.input()
-col1_f = open("col1.txt", "w")
-col2_f = open("col2.txt", "w")
-for line in f_in:
-    col = line.split("\t")
-    print(col[0], file=col1_f)
-    print(col[1], file=col2_f)
+with open("col1.txt", "w") as col1_f,\
+        open("col2.txt", "w") as col2_f,\
+        fileinput.input() as f_in:
+    for line in f_in:
+        col = line.split("\t")
+        print(col[0], file=col1_f)
+        print(col[1], file=col2_f)
 
 # cut -f1 hightemp.txt > col1.txt && cut -f2 hightemp.txt > col2.txt
