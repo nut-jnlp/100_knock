55b924df45e06ea6a99551dbca236beb427833c1
Maruyama <maruyama@jnlp.org>
2016-11-19 10:34:27 +0900

    100本ノック第1章03です。

---
+++ chap_1/ex03.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+s = """Now I need a drink, alcoholic of course, after the heavy lectures
+ involving quantum mechanics."""
+s = s.replace(",", "").replace(".", "")
+s = s.split(' ')
+print(s)
+
+num = list((len(s[a]) for a in range(15)))
+print(num)
