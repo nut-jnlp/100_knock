2393fbe62d2384e54dfd2c2716f6c5fb9c3f26f7
inaoka <in@okayu.jp>
2016-11-19 10:36:57 +0900

    内包表記を用いて一括でdictを生成

--- chap01/q04.py
+++ chap01/q04.py
@@ -6,10 +6,9 @@ New Nations Might Also Sign Peace Security Clause. Arthur King Can."""
 top_char = {1, 5, 6, 7, 8, 9, 15, 16, 19}
 
 words = sentence.replace(",", "").replace(".", "").split(" ")
-elms = map(
-    lambda i_w: i_w[1][:1] if i_w[0]+1 in top_char else i_w[1][:2],
-    enumerate(words)
+elms = dict(
+    (w[:1], i+1) if i+1 in top_char else (w[:2], i+1)
+    for i, w in enumerate(words)
 )
-elms_dic = {elm: i+1 for i, elm in enumerate(elms)}
 
-print(elms_dic)
+print(elms)
