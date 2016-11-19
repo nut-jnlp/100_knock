9f73eb78ffc2ad51ddef1a366931681c98ac3301
inaoka <in@okayu.jp>
2016-11-19 09:34:41 +0900

    q04 を要件通り動くところまで実装

--- chap01/q04.py
+++ chap01/q04.py
@@ -1,7 +1,18 @@
 # coding: utf-8
 
 sentence = """\
-"Hi He Lied Because Boron Could Not Oxidize Fluorine. \
+Hi He Lied Because Boron Could Not Oxidize Fluorine. \
 New Nations Might Also Sign Peace Security Clause. Arthur King Can."""
+top_char = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
 words = sentence.replace(",", "").replace(".", "").split(" ")
+elms = map(
+    lambda i, w: w[:1] if i+1 in top_char else w[:2],
+    range(len(words)), words
+)
+elms_dic = dict(map(
+    lambda i, w: (w, i+1),
+    range(len(words)), elms
+))
+
+print(elms_dic)
