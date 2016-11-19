c971cf546c29b05ac51a0f344ae299f418158618
takahashi <takahashi@jnlp.org>
2016-11-19 13:49:44 +0900

    03を解答

--- chap_01/nlp100_chap1.py
+++ chap_01/nlp100_chap1.py
@@ -10,7 +10,7 @@ def solve_00():
     文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
     """
     word = 'stressed'
-    print(word[::-1])
+    return word[::-1]
 
 
 def solve_01():
@@ -19,7 +19,7 @@ def solve_01():
     「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
     """
     word = "パタトクカシーー"
-    print(word[::2])
+    return word[::2]
 
 
 def solve_02():
@@ -30,9 +30,23 @@ def solve_02():
     word1 = 'パトカー'
     word2 = 'タクシー'
     chars = chain.from_iterable(zip(word1, word2))
-    print(''.join(chars))
+    return ''.join(chars)
+
+
+def solve_03():
+    """
+    03. 円周率
+    "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+    """
+    sentence = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+    sentence = sentence.replace(',', '').replace('.', '')
+    words = sentence.split(' ')
+
+    charcter_nums = [len(word) for word in words]
+    return charcter_nums
 
 if __name__ == "__main__":
-    solve_00()
-    solve_01()
-    solve_02()
+    print(solve_00())
+    print(solve_01())
+    print(solve_02())
+    print(solve_03())
