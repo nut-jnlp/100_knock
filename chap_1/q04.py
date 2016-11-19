50441a1e3089a2a16372e42ff076b9329fe4eff2
Ryusei-KAKUBARI <kakubari.ryusei@gmail.com>
2016-11-19 11:35:30 +0900

    100本ノック　1章 問04.　変数strの名前をwordに変更

--- chap_1/q04.py
+++ chap_1/q04.py
@@ -7,9 +7,9 @@ word_list = ""
 number = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
 word_list = sentence.replace(".", "").split()
-for i, str in enumerate(word_list):
+for i, word in enumerate(word_list):
     if i+1 in number:
-        periodic_table[i+1] = str[:1:1]
+        periodic_table[i+1] = word[:1:1]
     else:
-        periodic_table[i+1] = str[:2:1]
+        periodic_table[i+1] = word[:2:1]
 print(periodic_table)
