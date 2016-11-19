b833ccdec43d90f4fe08ce552f02fa63fe370eca
takahashi <takahashi@jnlp.org>
2016-11-19 15:44:00 +0900

    13を解答

--- chap_02/nlp100_chap2.py
+++ chap_02/nlp100_chap2.py
@@ -45,7 +45,24 @@ def solve_12():
             fout_col1.writelines([cols[0], '\n'])
             fout_col2.writelines([cols[1], '\n'])
 
+
+def solve_13():
+    """
+    13. col1.txtとcol2.txtをマージ
+    12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
+
+    paste col1.txt col2.txt
+    """
+    with open('./col1.txt', 'r') as fin_col1, open('./col2.txt', 'r') as fin_col2, \
+            open('./merged_col.txt', 'w') as fout:
+        for line1, line2 in zip(fin_col1, fin_col2):
+            line1 = line1.rstrip()
+            line2 = line2.rstrip()
+
+            fout.writelines([line1, '\t', line2, '\n'])
+
 if __name__ == "__main__":
     # print(solve_10())
     # print(solve_11())
-    solve_12()
+    # solve_12()
+    solve_13()
