0f113d19c3caccae78d01b3fae286a4b1ed3028d
youichiro <cinnamon416@gmail.com>
2016-11-19 11:14:37 +0900

    zip関数を使ってループ

--- chap_01/q02.py
+++ chap_01/q02.py
@@ -1,8 +1,16 @@
 # -*- coding uft-8 -*-
 
+word = ""
 s1 = "パトカー"
 s2 = "タクシー"
 
+'''
 for i in range(4):
 	print(s1[i]+s2[i], end='')
-print('')
+print()
+'''
+
+# zip関数：複数のシーケンスをまとめてループ
+for s1,s2 in zip(s1,s2):
+	print(s1+s2,end="")
+print("")
