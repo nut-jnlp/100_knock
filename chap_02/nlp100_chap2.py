9ce05fa42e8c86d7a050368f8da2400236a738dd
takahashi <takahashi@jnlp.org>
2016-11-19 15:28:03 +0900

    11を解答

--- chap_02/nlp100_chap2.py
+++ chap_02/nlp100_chap2.py
@@ -17,5 +17,19 @@ def solve_10():
         line_num += 1
     return line_num
 
+
+def solve_11():
+    """
+    11. タブをスペースに置換
+    タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
+
+    cat hightemp.txt|tr $'\t' ' '
+    """
+    repl = lambda x: x.rstrip('\n').replace('\t', ' ')
+    lines = (repl(line) for line in sys.stdin)
+
+    return '\n'.join(lines)
+
 if __name__ == "__main__":
-    print(solve_10())
+    # print(solve_10())
+    print(solve_11())
