16424b457261e0e1fc04aff47d2627019d861796
katsuta <katsuta@jnlp.org>
2016-11-19 14:55:34 +0900

    修正：文字数のリストを追加しました。

--- chap1/p_03.py
+++ chap1/p_03.py
@@ -1,5 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 word = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-print (word.split(" "))
+Words = ""
+count = []
+words = word.replace(",","").replace(".","").split(" ")
+for i in range(len(words)):
+	count.append(len(words[i]))
+print(count)
+
 
