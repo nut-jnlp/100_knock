f59ee59d79a3697394fe04803aef57420e69c720
Ryusei-KAKUBARI <kakubari.ryusei@gmail.com>
2016-11-19 11:31:38 +0900

    100本ノック　1章 問05.　単語bi_gram及び文字bi_gramの訂正

--- chap_1/q05.py
+++ chap_1/q05.py
@@ -5,13 +5,19 @@ sentence = "I am an NLPer"
 
 def n_gram(sentence):
     #単語bi_gram
-    bi_gram = sentence.split()
-    print(bi_gram)
+    result = []
+    word_list = sentence.split()
+    for i in range(len(word_list)):
+        bi_gram = (word_list[i:i+2])
+        result.append(bi_gram)
+    print("単語bi_gram\n{0}\n".format(result))
 
     #文字bi_gram
-    bi_gram = []
+    result = []
     word_list = sentence.replace(" ", "")
-    bi_gram = list(word_list)
-    print(bi_gram)
+    for i in range(len(word_list)):
+        bi_gram = (word_list[i:i+2])
+        result.append(bi_gram)
+    print("文字bi_gram\n{0}".format(result))
 
 n_gram(sentence)
