689b5583c1b36ac0098a89e44bcb630023f477a4
inaoka <in@okayu.jp>
2016-11-25 10:49:32 +0900

    q14 を解答

---
+++ chap02/q14.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+import sys
+import fileinput
+
+args = sys.argv
+n = int(args[1])
+path = args[2]
+
+with open(path, "r") as fin:
+    for i in range(n):
+        line = fin.readline()
+        if not line:
+            break
+        print(line, end="")
+# python q14.py 4 col1.txt
+# head -n 4 col1.txt
