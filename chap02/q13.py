db9409b30b91f0021e87b877e49ca261c53e40d0
inaoka <in@okayu.jp>
2016-11-28 18:52:24 +0900

    タブ区切りの出力をsepにタブを指定する方法に修正

--- chap02/q13.py
+++ chap02/q13.py
@@ -4,5 +4,5 @@ with open("col1.txt", "r") as col1_f,\
         open("col2.txt", "r") as col2_f,\
         open("col12.txt", "w") as col12_f:
     for col1, col2 in zip(col1_f, col2_f):
-        print("{}\t{}".format(col1.rstrip(), col2.rstrip()), file=col12_f)
+        print(col1.rstrip(), col2.rstrip(), sep="\t", file=col12_f)
 # paste col1.txt col2.txt
